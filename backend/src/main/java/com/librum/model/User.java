3c80a180fd5f9dbb7c0b9243e50b3d67e667ce8b
anthyer <antonio_marcosph@hotmail.com>
2026-04-30 21:55:00 -0300

    feat(model): adiciona construtores a entidade User

--- backend/src/main/java/com/librum/model/User.java
+++ backend/src/main/java/com/librum/model/User.java
@@ -30,6 +30,15 @@ public class User {
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
 
+    public User() {}
+
+    public User(UUID id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
     public UUID getId() { return id; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }

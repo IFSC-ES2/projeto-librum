5b0d89b17564534a7974b4cbc6c472c74d59159b
anthyer <antonio_marcosph@hotmail.com>
2026-05-19 21:54:56 -0300

    feat(model): adiciona entidade QuizQuestion com mapeamento JPA e repository

---
+++ backend/src/main/java/com/librum/repository/QuizQuestionRepository.java
@@ -0,0 +1,10 @@
+package com.librum.repository;
+
+import com.librum.model.QuizQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuizQuestionRepository extends JpaRepository<QuizQuestion, Long> {
+    List<QuizQuestion> findByPhaseIdOrderByOrderIndex(Long phaseId);
+}
